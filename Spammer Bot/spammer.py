7c55e4207a1b54b66c00a0bee9e1b1f05daf7cde
Mareczek322 <75751507+Mareczek322@users.noreply.github.com>
2021-02-13 09:08:23 -0500

    Add files via upload

---
+++ Spammer Bot/spammer.py
@@ -0,0 +1,23 @@
+import pyautogui
+import time
+
+rec = input("Enter who to mention: ")
+msg = input("Enter the message: ")
+n = input("How many times ?: ")
+
+print("t minus")
+
+count = 5
+while(count != 0):
+	print(count)
+	time.sleep(1)
+	count -= 1
+
+print("Fire in the hole!!!")
+
+for i in range(0,int(n)):
+	pyautogui.typewrite(rec)
+	pyautogui.press('enter')
+	pyautogui.typewrite(msg)
+	pyautogui.press('enter')
+	time.sleep(1)
